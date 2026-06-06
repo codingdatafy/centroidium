dd27112de9c3caa0cc3a130640687ca077c00d6c
omarghrida <omarghrida11@gmail.com>
2026-06-06 15:44:43 +0000

    chore(typescript): ensure nextjs auto-generated environment types blueprint #1

---
+++ next-env.d.ts.ts
@@ -0,0 +1,12 @@
+/**
+ * @project CodingDatafy
+ * @license MIT
+ * @copyright 2026 CodingDatafy Organization
+ * @author CodingDatafy Team
+ */
+ 
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+// NOTE: This file should not be edited.
+// See https://nextjs.org/docs/app/api-reference/config/typescript for more information.
