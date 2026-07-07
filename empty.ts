15bd1e5cdc268ea7ecb59fae693da342b6972481
omarghrida <omarghrida11@gmail.com>
2026-07-07 14:37:23 +0000

    build(config): add empty.ts stub file to resolve fs module in browser #1

---
+++ empty.ts
@@ -0,0 +1,9 @@
+/**
+ * @project CodingDatafy
+ * @license MIT
+ * @copyright 2026 CodingDatafy Organization
+ * @author CodingDatafy Team
+ */
+
+// This file is intentionally left empty to stub node-only modules in browser environments.
+export {};
